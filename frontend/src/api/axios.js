3b3ed8d5d2a164a9f507353f6a4cb3d00199f8f3
Stephen Karikari <stephenkarikari@Stephens-MacBook-Pro.local>
2026-08-16 22:51:32 +0200

    Docker Update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://localhost:10000', // Replace with your backend URL
 });
 
 // Attach the JWT to every request once the user is logged in.
